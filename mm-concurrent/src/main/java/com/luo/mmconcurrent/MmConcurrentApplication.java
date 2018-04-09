019466dde1f73e6bc51b8f2ba51515dc765dace4
luo-office <luohong213@163.com>
2018-04-09 10:48:40 +0800

    add concurrent demo

---
+++ mm-concurrent/src/main/java/com/luo/mmconcurrent/MmConcurrentApplication.java
@@ -0,0 +1,12 @@
+package com.luo.mmconcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmConcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmConcurrentApplication.class, args);
+	}
+}
---
+++ mm-concurrent/src/test/java/com/luo/mmconcurrent/MmConcurrentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luo.mmconcurrent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MmConcurrentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
