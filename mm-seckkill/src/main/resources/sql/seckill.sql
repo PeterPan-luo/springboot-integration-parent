1f06c400db58300d0140ac253dd34cb7f04ea581
luo-office <luohong213@163.com>
2018-04-18 17:40:40 +0800

    add seckill sql

--- mm-seckkill/src/main/resources/sql/seckill.sql
+++ mm-seckkill/src/main/resources/sql/seckill.sql
@@ -10,7 +10,7 @@ Target Server Type    : MariaDB
 Target Server Version : 50556
 File Encoding         : 65001
 
-Date: 2018-04-18 16:05:59
+Date: 2018-04-18 17:40:02
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -55,23 +55,26 @@ CREATE TABLE `miaosha_goods` (
 -- Records of miaosha_goods
 -- ----------------------------
 INSERT INTO `miaosha_goods` VALUES ('1', '0', '10', '2018-04-18 08:56:41', '2018-04-19 14:43:20', '200.00');
-INSERT INTO `miaosha_goods` VALUES ('2', '2', '10', '2018-04-19 08:52:51', '2018-04-19 14:43:54', '200.00');
+INSERT INTO `miaosha_goods` VALUES ('2', '2', '8', '2018-04-18 11:30:41', '2018-04-19 14:43:54', '200.00');
 
 -- ----------------------------
 -- Table structure for miaosha_order
 -- ----------------------------
 DROP TABLE IF EXISTS `miaosha_order`;
 CREATE TABLE `miaosha_order` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_Id` bigint(20) DEFAULT NULL,
   `order_Id` bigint(20) DEFAULT NULL,
   `goods_Id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of miaosha_order
 -- ----------------------------
+INSERT INTO `miaosha_order` VALUES ('1', '15071027574', '5', '2');
+INSERT INTO `miaosha_order` VALUES ('2', '1', '1', '-5346144739450824145');
+INSERT INTO `miaosha_order` VALUES ('3', '15071027574', '6', '2');
 
 -- ----------------------------
 -- Table structure for miaosha_user
@@ -95,12 +98,12 @@ CREATE TABLE `miaosha_user` (
 INSERT INTO `miaosha_user` VALUES ('15071027574', 'luo', 'a5ef75bde574cf3b1154e4e484e51f87', '6c489a3441d24a6eafb04c9fc65c7103', null, '2018-04-18 08:16:43', '2018-04-18 14:16:38', null);
 
 -- ----------------------------
--- Table structure for orderInfo
+-- Table structure for order_info
 -- ----------------------------
-DROP TABLE IF EXISTS `orderInfo`;
-CREATE TABLE `orderInfo` (
-  `id` bigint(20) NOT NULL,
-  `user_Id` bigint(20) DEFAULT NULL,
+DROP TABLE IF EXISTS `order_info`;
+CREATE TABLE `order_info` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_Id` bigint(20) NOT NULL,
   `goods_Id` bigint(20) DEFAULT NULL,
   `delivery_AddrId` bigint(20) DEFAULT NULL,
   `goods_Name` varchar(32) DEFAULT NULL,
@@ -111,8 +114,11 @@ CREATE TABLE `orderInfo` (
   `create_Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `pay_Date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of orderInfo
+-- Records of order_info
 -- ----------------------------
+INSERT INTO `order_info` VALUES ('2', '11', '-5346144739450824145', null, 'test', '1', '10', '1', '0', '2018-04-18 17:21:18', '0000-00-00 00:00:00');
+INSERT INTO `order_info` VALUES ('5', '15071027574', '2', null, 'xiaomi', '1', '200', '1', '0', '2018-04-18 17:23:24', '0000-00-00 00:00:00');
+INSERT INTO `order_info` VALUES ('6', '15071027574', '2', null, 'xiaomi', '1', '200', '1', '0', '2018-04-18 17:30:41', '0000-00-00 00:00:00');
